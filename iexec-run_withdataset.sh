c71c0b57ccfa757e08aca00303b90c48fcac46d8
Eric Rodriguez <eric@MacBook-Air-de-Eric.local>
2025-06-27 12:06:20 +0200

    dataset ok

--- iexec-run_withdataset.sh
+++ iexec-run_withdataset.sh
@@ -2,7 +2,8 @@ iexec app run 0xbef8D1a3C51bB1374523e3C8D46C7fDC08d412dA \
   --chain bellecour \
   --wallet-file test_wallet \
   --params "{\"iexec_input_files\":[\"https://raw.githubusercontent.com/ericr6/teemagotchi/main/input_example/question.txt\"]}" \
-  --dataset 0x9FceD9428315FeB14bdB2Ab50032Ba2818b592A3 \
+  --dataset 0x96C2F2017d7A72f15fc5d71fBC9cE56134D2fa63 \
   --tag tee,tdx \
   --workerpool tdx-labs.pools.iexec.eth \
+  --encrypt-result \
   --watch
